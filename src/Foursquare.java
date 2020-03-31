73c5fa7094a28fc60e6fa3c3a20c87b220be32c9
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-03-31 16:28:29 -0700

    Draw 4 squares of different color using setRandomPenColor() and a for loop.

---
+++ src/Foursquare.java
@@ -0,0 +1,35 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class IntroToRobot {
+	public static void main(String args[]) {
+		Robot c3po = new Robot();
+		
+		c3po.penDown();
+		c3po.setPenWidth(20);
+		c3po.setSpeed(25);
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(-100);
+			c3po.turn(-90);
+		}
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(-100);
+			c3po.turn(90);
+		}
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(100);
+			c3po.turn(-90);
+		}
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(100);
+			c3po.turn(90);
+		}
+	}
+}
