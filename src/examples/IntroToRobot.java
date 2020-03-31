d4c4fe48bfef3352877efda6fadb437c589e109f
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-03-31 16:15:16 -0700

    Drew 4 squares with different colors using setRandomPenColor and afor loop.

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -3,16 +3,35 @@ package examples;
 import org.jointheleague.graphical.robot.Robot;
 
 public class IntroToRobot {
-	public static void main(String[] args) {
-
-		Robot r2d2 = new Robot();
-		r2d2.penDown();
-		r2d2.setPenWidth(20);
-		r2d2.setSpeed(5);
-		for (int i = 0; i < 4; i++) {
-			r2d2.move(100);
-			r2d2.turn(90);
+	public static void main(String args[]) {
+		Robot c3po = new Robot();
+		
+		c3po.penDown();
+		c3po.setPenWidth(20);
+		c3po.setSpeed(25);
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(-100);
+			c3po.turn(-90);
 		}
-
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(-100);
+			c3po.turn(90);
+		}
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(100);
+			c3po.turn(-90);
+		}
+		
+		c3po.setRandomPenColor();
+		for(int i=0;i < 4; i++) {
+			c3po.move(100);
+			c3po.turn(90);
+		}
 	}
 }
